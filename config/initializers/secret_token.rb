c0217b9f3cdfece0b351be22c6bd827c9500f521
Ahmed Farag <ahmed.farag1111@gmail.com>
2013-01-24 17:57:22 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveTabs::Application.config.secret_token = 'bd2755bd8d7d14c4b6e5eeab2d81cd4c084e1dff57b902462b09b7fc2e7c5c7af879702b5bfa3e1d00186911174af1370446d2b94a85ddda408aeca7ea830dac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SaveTabs::Application.config.session_store :cookie_store, key: '_saveTabs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SaveTabs::Application.config.session_store :active_record_store
